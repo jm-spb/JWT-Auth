be3ecd0259693420d361ddb1c93700745426d8ac
ivan <tuchel.ivan@gmail.com>
2022-05-26 09:02:16 +0300

    create routes

---
+++ server/router/index.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import userController from '../controllers/user-controller.js'
+
+const router = new Router();
+
+router.post('/registration', userController.registration);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/activate/:link', userController.activate);
+router.get('/refresh', userController.refresh);
+router.get('/users', userController.getUsers);
+
+export default router;
