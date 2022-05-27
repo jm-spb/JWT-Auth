f40e88ab67e9fc79d2a287ad99652a254a1940e3
ivan <tuchel.ivan@gmail.com>
2022-05-27 20:09:39 +0300

    create error middleware

---
+++ server/middlewares/error-middleware.js
@@ -0,0 +1,9 @@
+import ApiError from '../errors/api-error.js';
+
+export default function(err, req, res, next) {
+  console.log(err);
+  if (err instanceof ApiError) {
+    return res.status(err.status).json({message: err.message, errors: err.errors})
+  }
+  return res.status(500).json({message: "Unhandle error"})
+}
