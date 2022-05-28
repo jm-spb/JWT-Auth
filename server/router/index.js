a6674cf37de4b82cbb36f0207799d0420abff564
ivan <tuchel.ivan@gmail.com>
2022-05-28 11:31:47 +0300

    add email & password validation

--- server/router/index.js
+++ server/router/index.js
@@ -1,13 +1,18 @@
 import { Router } from 'express';
-import userController from '../controllers/user-controller.js'
+import userController from '../controllers/user-controller.js';
+import { body } from 'express-validator';
 
 const router = new Router();
 
-router.post('/registration', userController.registration);
+router.post(
+  '/registration',
+  body('email').isEmail(),
+  body('password').isLength({ min: 6, max: 16 }),
+  userController.registration,
+);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
-router.get('/users', userController.getUsers);
 
 export default router;
