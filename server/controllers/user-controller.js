5d1ecc1605bfefa408887e8239696691f55aec32
ivan <tuchel.ivan@gmail.com>
2022-05-31 10:45:38 +0300

    remove class UserController

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -3,76 +3,80 @@ require('dotenv').config();
 const userService = require('../service/user-service.js');
 const ApiError = require('../errors/api-error.js');
 
-class UserController {
-  async registration(req, res, next) {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return next(ApiError.BadRequset('Validation error', errors.array()));
-      }
-
-      const { email, password } = req.body;
-      const userData = await userService.registration(email, password);
-      res.cookie('refreshToken', userData.refreshToken, {
-        maxAge: 2592000000,
-        httpOnly: true,
-      });
-
-      return res.json(userData);
-    } catch (e) {
-      next(e);
+const userRegistration = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(ApiError.BadRequset('Validation error', errors.array()));
     }
-  }
 
-  async login(req, res, next) {
-    try {
-      const { email, password } = req.body;
-      const userData = await userService.login(email, password);
-      res.cookie('refreshToken', userData.refreshToken, {
-        maxAge: 2592000000,
-        httpOnly: true,
-      });
-      return res.json(userData);
-    } catch (e) {
-      next(e);
-    }
-  }
+    const { email, password } = req.body;
+    const userData = await userService.registration(email, password);
+    res.cookie('refreshToken', userData.refreshToken, {
+      maxAge: 2592000000,
+      httpOnly: true,
+    });
 
-  async logout(req, res, next) {
-    try {
-      const { refreshToken } = req.cookies;
-      await userService.logout(refreshToken);
-      res.clearCookie('refreshToken');
-      return res.sendStatus(200);
-    } catch (e) {
-      next(e);
-    }
+    return res.json(userData);
+  } catch (e) {
+    next(e);
   }
+};
 
-  async activate(req, res, next) {
-    try {
-      const activationLink = req.params.link;
-      await userService.activate(activationLink);
-      return res.redirect(process.env.CLIENT_URL);
-    } catch (e) {
-      next(e);
-    }
+const userActivate = async (req, res, next) => {
+  try {
+    const activationLink = req.params.link;
+    await userService.activate(activationLink);
+    return res.redirect(process.env.CLIENT_URL);
+  } catch (e) {
+    next(e);
   }
+};
 
-  async refresh(req, res, next) {
-    try {
-      const { refreshToken } = req.cookies;
-      console.log(refreshToken);
-      const userData = await userService.refresh(refreshToken);
-      res.cookie('refreshToken', userData.refreshToken, {
-        maxAge: 2592000000,
-        httpOnly: true,
-      });
-      return res.json(userData);
-    } catch (e) {
-      next(e);
-    }
+const userRefresh = async (req, res, next) => {
+  try {
+    const { refreshToken } = req.cookies;
+    console.log(refreshToken);
+    const userData = await userService.refresh(refreshToken);
+    res.cookie('refreshToken', userData.refreshToken, {
+      maxAge: 2592000000,
+      httpOnly: true,
+    });
+    return res.json(userData);
+  } catch (e) {
+    next(e);
   }
-}
+};
 
-module.exports = new UserController();
+const userLogin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const userData = await userService.login(email, password);
+    res.cookie('refreshToken', userData.refreshToken, {
+      maxAge: 2592000000,
+      httpOnly: true,
+    });
+    return res.json(userData);
+  } catch (e) {
+    next(e);
+  }
+};
+
+const userLogout = async (req, res, next) => {
+  try {
+    const { refreshToken } = req.cookies;
+    await userService.logout(refreshToken);
+    res.clearCookie('refreshToken');
+    return res.sendStatus(200);
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports = {
+  userRegistration,
+  userActivate,
+  userRefresh,
+  userLogin,
+  userLogout,
+};
