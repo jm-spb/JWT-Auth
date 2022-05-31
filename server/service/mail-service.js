10b1cb50f81c23bd0567d0b3baa2ec98d058ac8b
ivan <tuchel.ivan@gmail.com>
2022-05-31 11:55:35 +0300

    remove class MailService & change response html

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -1,33 +1,29 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-class MailService {
-  constructor() {
-    this.transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      secure: false,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
-  }
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
-  async sendActivationMail(to, link) {
-    await this.transporter.sendMail({
-      from: process.env.SMTP_USER,
-      to,
-      subject: 'Account activation on ' + process.env.API_URL,
-      text: '',
-      html: `
-        <div>
-          <h1>For activation go to</h1>
-          <a href="${link}">${link}</a>
-        </div>
-      `,
-    });
-  }
-}
+const sendActivationMail = async (to, link) => {
+  await transporter.sendMail({
+    from: process.env.SMTP_USER,
+    to,
+    subject: `Account activation on ${process.env.API_URL}`,
+    text: 'Account activation',
+    html: `
+      <div>
+        <h1>Click the link below to activate your account</h1>
+        <a href="${link}">Activate Account</a>
+      </div>
+    `,
+  });
+};
 
-module.exports = new MailService();
+module.exports = sendActivationMail;
