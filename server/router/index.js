632a1b3882a1ef6708d1c7cc6b74d5b5ede3917f
ivan <tuchel.ivan@gmail.com>
2022-05-31 10:48:51 +0300

    add controllers to router

--- server/router/index.js
+++ server/router/index.js
@@ -1,18 +1,22 @@
-const Router = require('express').Router;
+const router = require('express').Router();
 const { body } = require('express-validator');
-const userController = require('../controllers/user-controller.js');
-
-const router = new Router();
+const {
+  userRegistration,
+  userActivate,
+  userRefresh,
+  userLogin,
+  userLogout,
+} = require('../controllers/user-controller.js');
 
 router.post(
   '/registration',
   body('email').isEmail(),
   body('password').isLength({ min: 6, max: 16 }),
-  userController.registration,
+  userRegistration,
 );
-router.post('/login', userController.login);
-router.post('/logout', userController.logout);
-router.get('/activate/:link', userController.activate);
-router.get('/refresh', userController.refresh);
+router.post('/login', userLogin);
+router.post('/logout', userLogout);
+router.get('/activate/:link', userActivate);
+router.get('/refresh', userRefresh);
 
 module.exports = router;
