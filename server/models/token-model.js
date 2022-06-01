241da9c0825c95f66621b86b2c71214d0b816e5f
ivan <tuchel.ivan@gmail.com>
2022-06-01 15:54:37 +0300

    add expiring time in token model

--- server/models/token-model.js
+++ server/models/token-model.js
@@ -3,6 +3,11 @@ const { Schema, model } = require('mongoose');
 const TokenSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User' },
   refreshToken: { type: String, required: true },
+  createdAt: {
+    type: Date,    
+    default: Date.now,
+    expires: '30d',
+  },
 });
 
 module.exports = model('Token', TokenSchema);
