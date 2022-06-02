12430c3551e342e1d6817c53b6b9532b8ea018a6
ivan <tuchel.ivan@gmail.com>
2022-06-02 11:25:20 +0300

    add Spinner

---
+++ client/src/components/Spinner.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SpinnerDiamond } from 'spinners-react';
+
+const Spinner = (): JSX.Element => (
+  <SpinnerDiamond size={70} speed={150} color="#ffffff" secondaryColor="#000000" />
+);
+
+export default Spinner;
