1a3fd4a02bcc4986d9e615fecc0d9f43cdd98bc3
ivan <tuchel.ivan@gmail.com>
2022-06-03 12:11:08 +0300

    create private methods

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,24 +1,27 @@
 import axios from 'axios';
 import { makeAutoObservable } from 'mobx';
 import AuthService from '../services/AuthService';
-import { IAuthResponse } from '../types/IAuthResponse';
-import { IUser } from '../types/IUser';
+import { IAuthResponse, IUser } from '../types';
 
 export default class Store {
   user = {} as IUser;
   isAuth = false;
-  isLoading = false;
+  isLoading = true;
+  apiError = '';
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  setAuth(bool: boolean) {
+  private _setAuth(bool: boolean) {
     this.isAuth = bool;
   }
-
-  setUser(user: IUser) {
+  private _setUser(user: IUser) {
     this.user = user;
+  }
+
+  private _setApiError(msg: string) {
+    this.apiError = msg;
   }
 
   setIsLoading(bool: boolean) {
@@ -30,10 +33,11 @@ export default class Store {
       const response = await AuthService.login(email, password);
       console.log(response);
       localStorage.setItem('token', response.data.accessToken);
-      this.setAuth(true);
-      this.setUser(response.data.user);
+      this._setAuth(true);
+      this._setUser(response.data.user);
     } catch (err) {
       const error = err as any;
+      this._setApiError(error.response?.data?.message);
       console.log(error.response?.data?.message);
     }
   }
@@ -43,11 +47,12 @@ export default class Store {
       const response = await AuthService.registration(email, password);
       console.log(response);
       localStorage.setItem('token', response.data.accessToken);
-      this.setAuth(true);
-      this.setUser(response.data.user);
+      this._setAuth(true);
+      this._setUser(response.data.user);
     } catch (err) {
       const error = err as any;
-      console.log(error.response.data.message);
+      this._setApiError(error.response?.data?.message);
+      console.log(error.response?.data?.message);
     }
   }
 
@@ -55,10 +60,11 @@ export default class Store {
     try {
       await AuthService.logout();
       localStorage.removeItem('token');
-      this.setAuth(false);
-      this.setUser({} as IUser);
+      this._setAuth(false);
+      this._setUser({} as IUser);
     } catch (err) {
       const error = err as any;
+      this._setApiError(error.response?.data?.message);
       console.log(error.response.data.message);
     }
   }
@@ -71,10 +77,11 @@ export default class Store {
         { withCredentials: true },
       );
       localStorage.setItem('token', response.data.accessToken);
-      this.setAuth(true);
-      this.setUser(response.data.user);
+      this._setAuth(true);
+      this._setUser(response.data.user);
     } catch (err) {
       const error = err as any;
+      this._setApiError(error.response?.data?.message);
       console.log(error.response.data.message);
     } finally {
       this.setIsLoading(false);
