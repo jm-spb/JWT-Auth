227ce85c56ca26e73865fd23ea3eeff6839f31b4
ivan <tuchel.ivan@gmail.com>
2022-11-21 21:34:20 +0300

    add mobx observer, check localStorage for token

--- client/src/components/RequireAuth.tsx
+++ client/src/components/RequireAuth.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { observer } from 'mobx-react-lite';
 import { Navigate, useLocation } from 'react-router-dom';
 import { Context } from '..';
+import Spinner from './Spinner';
 
 const RequireAuth = ({ children }: { children: React.ReactNode }): JSX.Element => {
+  console.log('Require Auth');
   const { store } = React.useContext(Context);
   const location = useLocation();
+
+  React.useEffect(() => {
+    if (localStorage.getItem('token')) {
+      console.log('hi');
+      store.checkAuth();
+    } else {
+      store.setIsLoading(false);
+    }
+  }, []);
+
+  console.log(store.isAuth);
+  console.log(store.isLoading);
+
+  if (store.isLoading) {
+    return <Spinner />;
+  }
 
   if (!store.isAuth) {
     return <Navigate to="/account/login" replace state={{ path: location.pathname }} />;
@@ -13,4 +32,4 @@ const RequireAuth = ({ children }: { children: React.ReactNode }): JSX.Element =
   return <>{children}</>;
 };
 
-export default RequireAuth;
+export default observer(RequireAuth);
