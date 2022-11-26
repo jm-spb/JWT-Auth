fc52e68628ce042f192aa0c20fb7c2fa72efc943
ivan <tuchel.ivan@gmail.com>
2022-11-26 23:00:59 +0300

    SERVER: remove express validator, add auth middleware

--- server/router/index.js
+++ server/router/index.js
@@ -1,17 +1,12 @@
 const router = require('express').Router();
-const { body } = require('express-validator');
 const userController = require('../controllers/user-controller.js');
+const authMiddleware = require('../middlewares/auth-middleware.js');
 
-router.post(
-  '/registration',
-  body('email').isEmail(),
-  body('password').isLength({ min: 6, max: 16 }),
-  userController.registration
-);
+router.post('/registration', userController.registration);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
-router.get('/users', userController.fetchUsers);
+router.get('/users', authMiddleware, userController.fetchUsers);
 
 module.exports = router;
