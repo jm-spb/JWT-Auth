6e14d87f755dd697974189057835313a40d6184c
ivan <tuchel.ivan@gmail.com>
2022-11-29 15:09:43 +0300

    CLIENT: add axios interceptors

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosRequestHeaders } from 'axios';
+import { IAuthResponse } from '../types';
 
 const api = axios.create({
   withCredentials: true,
@@ -10,5 +11,28 @@ api.interceptors.request.use((config) => {
   headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const prevRequest = error?.config;
+    if (error?.response?.status === 401 && !prevRequest?._sent) {
+      prevRequest._sent = true;
+      try {
+        const response = await axios.get<IAuthResponse>(
+          `${process.env.REACT_APP_API_URL}/api/refresh`,
+          { withCredentials: true }
+        );
+        // prevRequest.headers['Authorization'] = `Bearer ${response.data.accessToken}`;
+        localStorage.setItem('token', response.data.accessToken)
+        return api.request(prevRequest);
+      } catch (error) {
+        console.log('NE AVTORIZOVAN');
+      }
+    }
+
+    return Promise.reject(error)
+  }
+);
 
 export default api;
