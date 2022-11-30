fe520c11553033c9d15b1423aac5b838dd1edbdb
ivan <tuchel.ivan@gmail.com>
2022-11-30 21:54:02 +0300

    CLIENT: create Register Page functionality

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { observer } from 'mobx-react-lite';
+import { useNavigate } from 'react-router-dom';
+import { Context } from '..';
+import Form from '../components/Form';
+import { IFormInputs } from '../types';
 
-const RegisterPage = (): JSX.Element => <div>Register</div>;
+const RegisterPage = (): JSX.Element => {
+  const { store } = React.useContext(Context);
+  const navigate = useNavigate();
 
-export default RegisterPage;
+  const onSubmit = async ({ email, password }: IFormInputs) => {
+    await store.registration(email, password);
+    // TODO: if registrationIsSuccess -> navigate
+    navigate('/account/login');
+  };
+
+  const apiErrorMsg = store.apiError ? (
+    <div className="container__api-error">{store.apiError}</div>
+  ) : null;
+
+  return (
+    <Form formName="Sign Up" apiErrorMsg={apiErrorMsg} onSubmit={onSubmit} registerPage />
+  );
+};
+
+export default observer(RegisterPage);
