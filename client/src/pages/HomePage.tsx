8cd810b15b4787a4ca34b25b1099ce90a6c7de2b
ivan <tuchel.ivan@gmail.com>
2022-12-05 09:32:37 +0300

    CLIENT: add hahdleFetchUsers func

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
 import { Context } from '..';
-import Spinner from '../components/Spinner';
+import UserService from '../services/UserService';
+import { IUser } from '../types';
 
 const HomePage = (): JSX.Element => {
-  React.useEffect(() => {
-    if (localStorage.getItem('token')) {
-      console.log('hi');
-      store.checkAuth();
-    }
-  }, []);
-
+  const [allUsers, setAllUsers] = useState<IUser[]>([]);
   const { store } = React.useContext(Context);
 
-  if (store.isLoading) {
-    return <Spinner />;
-  }
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const handleLogout = () => store.logout();
+  const handleFetchUsers = async () => {
+    try {
+      const response = await UserService.fetchAllUsers();
+      console.log(response);
+      setAllUsers(response.data);
+    } catch (error) {
+      console.log(error);
+      navigate('/account/login', { replace: true, state: { from: location } });
+    }
+  };
 
   return (
     <main>
       <h1>{store.isAuth ? `User is auth ${store.user.userEmail}` : 'Unauthorized'}</h1>
       <button onClick={handleLogout}>Logout</button>
+      <button onClick={handleFetchUsers}>Fetch Users</button>
+      <ul>
+        {allUsers.map(({ userEmail }) => (
+          <li key={userEmail}>{userEmail}</li>
+        ))}
+      </ul>
     </main>
   );
 };
