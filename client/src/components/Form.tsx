6a16ded501dd360f79a690139e8cdb3f0fb7039b
ivan <tuchel.ivan@gmail.com>
2022-12-07 22:50:57 +0300

    CLIENT: adding functionality & refactoring - remove password validation from component - disable button & add spinner on interactions with server - add toggle password visibility

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Eye, EyeOff } from 'react-feather';
 import '../styles/LoginForm.scss';
 import { IFormInputs, IFormProps } from '../types';
+import { ButtonSpinner } from './Spinners';
 
 const Form = ({
   formName,
   apiErrorMsg,
   onSubmit,
-  signUpSuggestion,
+  bottomNavigation,
   registerPage,
+  isLoading,
+  passwordValidation,
 }: IFormProps): JSX.Element => {
   const {
     register,
@@ -26,8 +30,14 @@ const Form = ({
     }
   }, [isSubmitSuccessful, reset]);
 
+  const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
+
   const password = React.useRef({});
   password.current = watch('password', '');
+
+  const toggleShowPassword = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
 
   return (
     <article className="container">
@@ -51,48 +61,24 @@ const Form = ({
         ) : (
           <span className="form__empty" />
         )}
-
         <label className="form__label" htmlFor="password">
           Password
           <input
-            {...register('password', {
-              required: 'Password field is empty',
-              /* Password should contain at least: 
-              - 1 digit
-              - 1 small-case letter
-              - 1 capital letter
-              - 1 special character
-              - length: 6-10 characters
-              - order not important
-              1A2a$5              
-              */
-              minLength: {
-                value: 6,
-                message: 'Password length should be more than 5 characters',
-              },
-              maxLength: {
-                value: 10,
-                message: 'Password length should be less than 11 characters',
-              },
-              pattern: {
-                value:
-                  /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/gm,
-                message:
-                  'Password must have at least: one number, letter, capital letter, special character',
-              },
-            })}
+            {...register('password', passwordValidation)}
             className="form__input"
             id="password"
-            type="password"
+            type={isPasswordVisible ? 'text' : 'password'}
             placeholder="Enter password"
           />
+          <span className="form__togglePassword" onClick={toggleShowPassword}>
+            {isPasswordVisible ? <Eye /> : <EyeOff />}
+          </span>
         </label>
         {errors?.password ? (
           <span className="form__error">{errors?.password.message}</span>
         ) : (
           <span className="form__empty" />
         )}
-
         {registerPage ? (
           <>
             <label className="form__label" htmlFor="passwordConfirm">
@@ -116,12 +102,18 @@ const Form = ({
             )}
           </>
         ) : null}
-
-        <button className="btn form__submit" type="submit" disabled={!isValid}>
-          {formName}
+        <button
+          className="btn form__submit"
+          type="submit"
+          disabled={!isValid || isLoading}
+        >
+          <span className="btn__content">
+            <span>{formName}</span>
+            {isLoading ? <ButtonSpinner /> : null}
+          </span>
         </button>
       </form>
-      {signUpSuggestion}
+      {bottomNavigation}
     </article>
   );
 };
