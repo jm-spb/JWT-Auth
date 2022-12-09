6115fa2704abbbde9129b1ad88591d2b9ac4e6fa
ivan <tuchel.ivan@gmail.com>
2022-12-09 15:39:04 +0300

    CLIENT: adjust project settings - index.tsx: add types, BrowserRouter - delete styles.css - update .gitignore - update package.json

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/index.scss';
 import App from './App';
 import Store from './store';
 
@@ -12,9 +14,9 @@ export const Context = React.createContext<IStoreState>({ store });
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  // <React.StrictMode>
   <Context.Provider value={{ store }}>
-    <App />
-  </Context.Provider>,
-  // </React.StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Context.Provider>
 );
