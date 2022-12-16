c55df7a120a75a7aaeeb5cfdf98c037fa845de62
jm-spb <111maverick111@gmail.com>
2022-12-16 17:18:20 +0300

    SERVER: change token TTL value to 60sec

--- server/models/token-model.js
+++ server/models/token-model.js
@@ -4,10 +4,17 @@ const TokenSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User' },
   refreshToken: { type: String, required: true },
   createdAt: {
-    type: Date,    
-    default: Date.now,
-    expires: '30d',
+    type: Date,
+    default: new Date(),
   },
 });
+
+/* 
+! For testing purposes time to delete token from DB is set to 60sec
+* in order to change "expireAfterSeconds" in prod also need: 
+* https://stackoverflow.com/questions/64830437/document-not-expiring-in-mongodb-using-mongoose
+* + delete/rename collection in DB
+*/
+TokenSchema.index({ createdAt: 1 }, { expireAfterSeconds: 60 });
 
 module.exports = model('Token', TokenSchema);
