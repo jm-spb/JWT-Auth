3e56cb8b9f25d9fb9620054b6136b8a3d255e6ac
ivan <tuchel.ivan@gmail.com>
2023-02-23 20:14:02 +0300

    CLIENT: set isActivated flag

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -7,14 +7,14 @@ import { IUser } from '../types';
 import { ButtonSpinner } from '../components/Spinners';
 
 const HomePage = (): JSX.Element => {
-  const [allUsers, setAllUsers] = useState<IUser[]>([]);
+  const [allUsers, setAllUsers] = useState<IUser[] | null>(null);
   const { store } = React.useContext(Context);
+  const { isActivated } = store.user;
 
   const navigate = useNavigate();
   const location = useLocation();
 
-  const handleLogout = () => store.logout();
-
+  const handleLogout = async () => await store.logout();
   const handleFetchUsers = async () => {
     const response = await store.fetchUsers();
     if (response) {
@@ -27,28 +27,40 @@ const HomePage = (): JSX.Element => {
   return (
     <main className="home">
       <div className="home__top">
-        <h1 className="home__heading">
-          {store.isAuth ? `User: ${store.user.userEmail}` : 'Unauthorized'}
-        </h1>
+        <h1 className="home__heading">{`User: ${store.user.userEmail}`}</h1>
       </div>
       <div className="home__buttons">
-        <button className="btn" onClick={handleLogout}>
+        <button className="btn home__logout-btn" onClick={handleLogout}>
           Logout
         </button>
-        <button className="btn home__fetch-btn" onClick={handleFetchUsers}>
-          <span className="btn__content">
+        <button
+          className="btn home__fetch-btn"
+          onClick={handleFetchUsers}
+          disabled={!isActivated}
+        >
+          <div>
             <span>Fetch Users</span>
             {store.isLoadingUsersData ? <ButtonSpinner /> : null}
-          </span>
+          </div>
         </button>
       </div>
-      <div className="home__content">
-        <ul>
-          {allUsers.map(({ userEmail }) => (
-            <li key={userEmail}>{userEmail}</li>
-          ))}
-        </ul>
-      </div>
+      {!isActivated ? (
+        <span className="home__notActivated">
+          Your account has not been activated. In order to retrieve users e-mails, please
+          go to your e-mail inbox and activate your account.
+        </span>
+      ) : null}
+
+      {allUsers ? (
+        <div className="home__content content">
+          <h1 className="content__heading">All users emails:</h1>
+          <ul className="content__usersList">
+            {allUsers.map(({ userEmail }) => (
+              <li key={userEmail}>{userEmail}</li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
     </main>
   );
 };
